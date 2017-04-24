4bc73024be19997842cab34bb32519f5783672d1
motasim1 <elmotasimbillah@gmail.com>
2017-04-24 15:17:49 +0200

    Update selfbot.js

--- selfbot/selfbot.js
+++ selfbot/selfbot.js
@@ -133,7 +133,6 @@ bot.on('message', (message) => {
               .addField(config.prefix + 'serverinfo', 'Shows the serverinfo.')
               .addField(config.prefix + 'leave', 'Leaves the server where the message was sent in.')
               .addField(config.prefix + 'perms', 'Shows the permissions for you in a server.')
-              .addField(config.prefix + 'spam', 'Spams the server or DM')
               .addField('---------------------------------------------', "Made possible by Motasim's [selfbot](https://github.com/motasim1/selfbot)")
               message.channel.sendEmbed(embed)
             } else {
